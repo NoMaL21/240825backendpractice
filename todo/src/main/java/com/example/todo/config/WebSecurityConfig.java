51ef3c604958fd6dbc3bf93519f8df81ab7bc5b1
NoMaL21 <dntjd8509@naver.com>
2024-09-02 22:04:12 +0900

    JWT 토큰을 이용한 로그인 구현

---
+++ todo/src/main/java/com/example/todo/config/WebSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.example.todo.config;
+
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+public class WebSecurityConfig {
+
+}
